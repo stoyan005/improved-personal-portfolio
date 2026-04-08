516579091a6ed0f349d84d8b239313871b3a3b90
Stoyan <ivanovtoni814@gmail.com>
2026-04-08 14:49:52 +0100

    Included animation for underlined buttons for interactiveness and aesthetic feel

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,8 +1,50 @@
-import { useState } from React;
+import { useState } from "react";
 import styles from './Information.module.css';
 
 const Information = () => {
     const [active, setActive] = useState(null);
+    const [animate, setAnimate] = useState(null);
+
+    const toggle = (section) => {
+        setAnimate(false);
+
+        setTimeout(() => {
+            setActive(section);
+            setAnimate(true);
+        }, 50);
+    };
+
+    return (
+        <div className={styles.hero}>
+            <h1>STOYAN IVANOV</h1>
+            <p>WEB DEVELOPER • DIGITAL CREATOR • DATA ANALYST </p>
+
+
+            <div className={styles.contact}>
+                <button onClick={() => toggle("email")}>Email</button>
+                <button onClick={() => toggle("linkedin")}>LinkedIn</button>
+                <button onClick={() => toggle("projects")}>Github Projects</button>
+            </div>
+
+            <div className={`${styles.dropdown} ${animate ? styles.show : ""}`}>
+                {active === "email" && (
+                    <p>your@email.com</p>
+                )}
+
+                {active === "linkedin" && (
+                    <a href="https://github.com/stoyan005" target='_blank'>
+                        View LinkedIn Profile
+                    </a>
+                )}
+
+                {active === "projects" && (
+                    <a href='https://github.com/stoyan005' target='_blank'>
+                        View Github Projects
+                    </a>
+                )}
+            </div>
+        </div>
+    )
 };
 
 export default Information;
