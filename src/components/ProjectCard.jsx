0246403387d6fa1f1bf05bd6d3ba63203d12d177
Stoyan <ivanovtoni814@gmail.com>
2026-04-08 22:22:33 +0100

    added images to the projects cards (still a bit of fixing needed) and created a separate folder for images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,13 @@
 import styles from './ProjectCard.module.css';
 
-function ProjectCard({ title, description, link }) {
+function ProjectCard({ title, description, link, image }) {
     return (
         <div className={styles.project}>
+            {image && <img src={image} alt={title} className={styles.project_image} />}
+
             <h3>{title}</h3>
             <p>{description}</p>
+
             {link && (
                 <a href={link} target="_blank" rel="noopener noreferrer">
                     View here
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,24 @@ import styles from "./Projects.module.css";
 
 function Projects() {
     const projectData = [
-        { title: "Pazardzhik Bus Map", description: "An interactive web application designed to help users explore and navigate bus routes across Pazardzhik, Bulgaria with ease.", link: "https://github.com/stoyan005/pazardzhik-bus-map" },
-        { title: "SDI & Sons LTD.", description: "A sleek and responsive website that showcases services, and provides an easy way for clients to connect.", link: "https://sdi-sons.vercel.app/" },
-        { title: "Sky Engineering Project (WIP)", description: "A team management dashboard for Sky Engineering featuring real-time chat, team overviews, and tools to keep collaboration organized and efficient.", link: "https://github.com/stoyan005/Axiom-Sky-Engineering-Project" },
+        {
+            title: "Pazardzhik Bus Map",
+            description: "An interactive web application designed to help users explore and navigate bus routes across Pazardzhik, Bulgaria with ease.",
+            link: "https://github.com/stoyan005/pazardzhik-bus-map",
+            image: "/images/pazardzhik_logo.png"
+        },
+        {
+            title: "SDI & Sons LTD.",
+            description: "A sleek and responsive website that showcases services, and provides an easy way for clients to connect.",
+            link: "https://sdi-sons.vercel.app/",
+            image: "/images/SDI_logo_transparent.webp"
+        },
+        {
+            title: "Sky Engineering Project (WIP)",
+            description: "A team management dashboard for Sky Engineering featuring real-time chat, team overviews, and tools to keep collaboration organized and efficient.",
+            link: "https://github.com/stoyan005/Axiom-Sky-Engineering-Project",
+            image: "/images/work_in_progress.jpg"
+        }
     ];
 
     return (
@@ -18,6 +33,7 @@ function Projects() {
                         title={project.title}
                         description={project.description}
                         link={project.link}
+                        image={project.image}
                     />
                 ))}
             </div>
